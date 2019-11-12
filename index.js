1af848d35e0bad614076cfd72b02605d8447cee0
thethinh <nguuyenthethinh16011998@gmail.com>
2019-11-12 22:57:28 +0700

    fixed pug not load css

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ var port = 3000;
 
 app.set('view engine','pug');
 app.set('views','./views');
+app.use(express.static('public'));
 
 app.get('/',function(req,res){
 	res.render('index');
 });
 
+app.get('/KhoaCNTT',function(req,res){
+	res.render('KhoaCNTT');
+})
 app.listen(port, function(){
 	console.log('Server listening on port'+port);
 })
