ae5bf3c4a89647f7e8f64711ff5aba17d0de8e2b
thethinh <nguuyenthethinh16011998@gmail.com>
2019-11-12 22:03:11 +0700

    begin, but have still error

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+var port = 3000;
+
+app.set('view engine','pug');
+app.set('views','./views');
+
+app.get('/',function(req,res){
+	res.render('index');
+});
+
+app.listen(port, function(){
+	console.log('Server listening on port'+port);
+})
